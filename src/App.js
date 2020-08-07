4027e039b1bfe70257ccfa40f3fe5bd93e1006e0
Travis Kohlbeck <me+git@travisk.info>
2020-08-07 16:50:31 -0400

    Click button to get random number

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-	React.useEffect(() => {
-		const fetchRandomNumber = async () => {
-			try {
-				const response = await fetch(
-					`https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8`,
-				)
-				const json = await response.json()
-				console.log(json.data[0])
-			} catch (e) {
-				console.error(e.name, e.message)
-			}
+	const [randomNumber, setRandomNumber] = useState(0)
+
+	const fetchRandomNumber = async () => {
+		try {
+			const response = await fetch(
+				`https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8`,
+			)
+			const json = await response.json()
+			setRandomNumber(json.data[0])
+		} catch (e) {
+			console.error(e.name, e.message)
 		}
-		fetchRandomNumber()
-	})
+	}
+
 	return (
 		<div className='App'>
 			<h1>World Splitter</h1>
@@ -25,7 +25,8 @@ function App() {
 			<label>
 				WORLD B<input />
 			</label>
-			<button>SPLIT</button>
+			{randomNumber}
+			<button onClick={fetchRandomNumber}>SPLIT</button>
 		</div>
 	)
 }
