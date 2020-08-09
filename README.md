a0f5dc9d3ba0e34c19ffa1481c673d9c939dae0c
Travis Kohlbeck <me+git@travisk.info>
2020-08-09 18:50:38 -0400

    Add links to about page, and replace img with svg

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # ideas
 
-- put 'universe a/b' as placeholder
+- ~~put 'universe a/b' as placeholder~~
 - add button to generate random inputs (e.g. do the dishes!)
-- what will get your app far is polish . an app with polish is more likely to be shared . this could be an app in which you put a lot of design polish
-- make background go back and forth
-- be able to press back to go to splash page
-- link to bottom that says explanation
+- ~~what will get your app far is polish . an app with polish is more likely to be shared . this could be an app in which you put a lot of design polish~~
+- ~~make background go back and forth~~
+- ~~be able to press back to go to splash page~~
+- ~~link to bottom that says explanation~~
 - cite now-mainstream
 - add links and such to about page
 - store splash state in localStorage
 - BUG not really looping bg
-- say the world split ~X ns
+- ~~say the world split ~X ns~~
 - pre-load svgs
--- src/App.js
+++ src/App.js
@@ -57,23 +57,61 @@ function App() {
 		return (
 			<div id='splash'>
 				<blockquote id='quote'>
-					"The Many-Worlds Interpretation of quantum mechanics holds that there
-					are many worlds which exist in parallel at the same space and time as
-					our own. The existence of the other worlds makes it possible to remove
-					randomness and action at a distance from quantum theory and thus from
-					all physics."
+					"The{' '}
+					<a
+						target='_blank'
+						rel='noopener noreferrer'
+						href='https://en.wikipedia.org/wiki/Many-worlds_interpretation'
+					>
+						Many-Worlds Interpretation
+					</a>{' '}
+					of quantum mechanics holds that there are many worlds which exist in
+					parallel at the same space and time as our own. The existence of the
+					other worlds makes it possible to remove randomness and action at a
+					distance from quantum theory and thus from all physics."
 				</blockquote>
-				<div id='quote-by'>― Stanford Encyclopedia of Philosophy</div>
-				<div>
-					This app will contact a quantum random number generator located at The
-					Australian National University in Canberra, splitting the universe
-					(and yourself) into two versions. Which world will you end up in?
+				<div id='quote-by'>
+					―{' '}
+					<a
+						target='_blank'
+						rel='noopener noreferrer'
+						href='https://plato.stanford.edu/entries/qm-manyworlds/'
+					>
+						Stanford Encyclopedia of Philosophy
+					</a>
 				</div>
 				<div>
-					Unlike all other interpretations of quantum mechanics, the
-					now-mainstream Many-Worlds or "Everettian" interpretation doesn't
-					require adding non-physical phenomena to the world. It is only what
-					the math describing the universe tells us.
+					This app will contact a{' '}
+					<a
+						target='_blank'
+						rel='noopener noreferrer'
+						href='https://qrng.anu.edu.au/'
+					>
+						quantum random number generator
+					</a>{' '}
+					located at{' '}
+					<a
+						target='_blank'
+						rel='noopener noreferrer'
+						href='https://www.anu.edu.au/'
+					>
+						The Australian National University in Canberra
+					</a>
+					, splitting the universe (and yourself) into two versions. Which world
+					will you end up in?
+				</div>
+				<div>
+					Unlike all other interpretations of quantum mechanics, the{' '}
+					<a
+						target='_blank'
+						rel='noopener noreferrer'
+						href='https://en.wikipedia.org/wiki/Many-worlds_interpretation#Polls'
+					>
+						now-mainstream
+					</a>{' '}
+					Many-Worlds or "Everettian" interpretation doesn't require adding
+					non-physical phenomena to the world. It is only what the math
+					describing the universe tells us.
 				</div>
 				<div id='splash-button'>
 					<button onClick={() => setIsSplashVisible(false)}>CONTINUE</button>
@@ -88,7 +126,13 @@ function App() {
 			<form onSubmit={fetchrandomBoolean}>
 				<label>
 					<div className='earth'>
-						<img src='world-splitter/earth.svg' alt='World A' />
+						<svg viewBox='0 0 156 156'>
+							<circle cx='78' cy='78' r='78' fill='#235c96' />
+							<path
+								d='m8 78c0 15.6 5 29.8 13.6 41.4l-3.2-22.2zm15-43.4 9.2-2.2c7.8-11.4 18.8-17.4 28.8-19.2l-15.8 11 15.6-3.8 11.2-6.2-3.2-5.6c-18.4 2.4-34.8 12-45.8 26zm10.2 43.2 6.6 13.2 19.6 1.8 10.6 40h13l14.8-18-0.8-11.2c6.4-5 11.4-11.2 13.4-19.2l-10 2.4-13-20.4 3-2 12.8 18 12.6-11c-6.6-0.8-11-4-12.8-9.6l22.8 3.6 8.2 16.8 3.6-22.4 10.2 12c-2.6-29.6-23.8-54-51.8-61.4 0 6.8-3.8 12.4-9.2 15.6l-7.2-4.2-13.8 8 4.8 4.6 7.4-8.8-1.4 10.8c-9 0-13.4 1.4-19 4.4l-1.2 5.6-6.2-1.4-2.6 7 5.4 1.6c3-3.8 5.4-6.2 11.2-7.6l6 6.6-3.6 1.2 2.2 1.8 3.2-3.8-4.6-6.4c4.6 0 7.2 2.8 8.8 9.8l8.2-12 10 4.2-1 2.6-11.8 1v4.4l9 0.6-2.8 7.2c-16.4 0-21.2-0.4-24.6-7.8l-13.6-0.8c-10 6-15 13.8-16.4 23.2zm19.4-41.4 8 2v-8zm44.4 7.8 4-2.2 5.4 11.2-3.6 0.8zm2.2 79.8 3.6 1.4 6-13.8-7.6 4.8z'
+								fill='#94ba32'
+							/>
+						</svg>
 						<div>A</div>
 					</div>
 					<input
@@ -99,7 +143,13 @@ function App() {
 				</label>
 				<label>
 					<div className='earth'>
-						<img src='world-splitter/earth.svg' alt='World B' />
+						<svg viewBox='0 0 156 156'>
+							<circle cx='78' cy='78' r='78' fill='#235c96' />
+							<path
+								d='m8 78c0 15.6 5 29.8 13.6 41.4l-3.2-22.2zm15-43.4 9.2-2.2c7.8-11.4 18.8-17.4 28.8-19.2l-15.8 11 15.6-3.8 11.2-6.2-3.2-5.6c-18.4 2.4-34.8 12-45.8 26zm10.2 43.2 6.6 13.2 19.6 1.8 10.6 40h13l14.8-18-0.8-11.2c6.4-5 11.4-11.2 13.4-19.2l-10 2.4-13-20.4 3-2 12.8 18 12.6-11c-6.6-0.8-11-4-12.8-9.6l22.8 3.6 8.2 16.8 3.6-22.4 10.2 12c-2.6-29.6-23.8-54-51.8-61.4 0 6.8-3.8 12.4-9.2 15.6l-7.2-4.2-13.8 8 4.8 4.6 7.4-8.8-1.4 10.8c-9 0-13.4 1.4-19 4.4l-1.2 5.6-6.2-1.4-2.6 7 5.4 1.6c3-3.8 5.4-6.2 11.2-7.6l6 6.6-3.6 1.2 2.2 1.8 3.2-3.8-4.6-6.4c4.6 0 7.2 2.8 8.8 9.8l8.2-12 10 4.2-1 2.6-11.8 1v4.4l9 0.6-2.8 7.2c-16.4 0-21.2-0.4-24.6-7.8l-13.6-0.8c-10 6-15 13.8-16.4 23.2zm19.4-41.4 8 2v-8zm44.4 7.8 4-2.2 5.4 11.2-3.6 0.8zm2.2 79.8 3.6 1.4 6-13.8-7.6 4.8z'
+								fill='#94ba32'
+							/>
+						</svg>{' '}
 						<div>B</div>
 					</div>
 					<input
